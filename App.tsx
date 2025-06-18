786ba37a21a7ba091e08b2c157732510e572efd7
TranML <the44secrets@gmail.com>
2025-06-18 14:03:33 +0700

    make first changes + create dev build for Android

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{fontSize: 36, fontWeight: 'bold', color: 'white'}}>Language Learning App</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+});
